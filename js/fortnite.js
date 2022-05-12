0d6933b77cef9c6ed8fe5c6892ae4af43ffe22ab
itteamgamesite <105166862+itteamgamesite@users.noreply.github.com>
2022-05-12 16:10:22 +0200

    Add files via upload

---
+++ js/fortnite.js
@@ -0,0 +1,6 @@
+<script >
+function myfunction() {
+    alert("Done") ;
+}
+
+</script>
---
+++ js/mazen.js
@@ -0,0 +1,6 @@
+function show_pop(){
+    document.getElementById('pop').classList.add('open')
+}
+function hide_pop(){
+    document.getElementById('pop').classList.remove('open')
+}
