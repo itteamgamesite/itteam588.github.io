24c208481d5e50c853db38846f521c15efd679f5
itteamgamesite <105166862+itteamgamesite@users.noreply.github.com>
2022-05-12 17:13:23 +0200

    Update noby_js.js

--- js/noby_js.js
+++ js/noby_js.js
@@ -1,1 +1,21 @@
-window.alert("Welcome to our maps site");
+const cards = document.querySelectorAll(".imgcards");
+console.log(cards);
+for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("mouseover", function() {
+        setEffect(this);
+    });
+    cards[i].addEventListener("mouseout", resEffect);
+}
+
+function setEffect(el) {
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].setAttribute("style", "filter: blur(5px);");
+    }
+    el.removeAttribute("style");
+}
+
+function resEffect() {
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].removeAttribute("style");
+    }
+}
